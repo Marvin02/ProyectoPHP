0b6202a0aa420fe3f89477802398506c420e946f
Marvin Matamoros Bravo <Marvin02@users.noreply.github.com>
2024-07-30 17:01:01 -0600

    actualizar leer-stream-entrada.php

--- basico/leer-stream-entrada.php
+++ basico/leer-stream-entrada.php
@@ -6,11 +6,15 @@
  * @author Linea de Codigo (http://lineadecodigo.com)
  * @date   2-mayo-2013
  * @url    http://lineadecodigo.com/php/leer-el-stream-de-entrada-en-php/
- * @description C—mo leer el stream de entrada en PHP para poder recibir peticiones de tipo XML o JSON en nuestro servidor.
+ * @description Câ€”mo leer el stream de entrada en PHP para poder recibir peticiones de tipo XML o JSON en nuestro servidor.
  */
 
-// Una forma de acceder a todo el contenido que recibe
 $datos = file_get_contents('php://input');
-var_dump($datos);
+$datos_sanitized = htmlspecialchars($datos, ENT_QUOTES, 'UTF-8');
 
+if ($datos_sanitized) {
+    var_dump($datos_sanitized);
+} else {
+    echo "Invalid input data.";
+}
 ?>
